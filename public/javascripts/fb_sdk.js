ddb6e67d0b435d919160003c313f5893fe6c1c0e
Bobby Koller <kolbobby@gmail.com>
2013-03-09 15:55:40 -0500

    working on fb

--- public/javascripts/fb_sdk.js
+++ public/javascripts/fb_sdk.js
@@ -11,7 +11,7 @@ window.fbAsyncInit = function() {
 	// Additional initialization code such as adding Event Listeners goes here
 	FB.getLoginStatus(function(response) {
 		if (response.status === 'connected') {
-			alert("Connected");
+			testAPI();
 		} else if (response.status === 'not_authorized') {
 			login();
 		} else {
